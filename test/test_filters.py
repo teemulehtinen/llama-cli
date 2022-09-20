cec212fca2686e999ac6b7599274e69865f6096d
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2022-09-20 20:22:09 +0300

    Add tests

--- test/test_filters.py
+++ test/test_filters.py
@@ -18,6 +18,16 @@ SOURCES = [
       }
     ],
   },
+  {
+    'id': 1,
+    'tables': [
+      {
+        'id': 'T3',
+        'name': 'T3',
+        'columns': [],
+      },
+    ],
+  },
 ]
 
 def table_ids(sources):
@@ -34,15 +44,23 @@ class TestFilters(unittest.TestCase):
 
   def test_empty(self):
     fl = Filters()
-    self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), table_ids(SOURCES))
+    self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), ['T1', 'T2', 'T3'])
 
-  def test_exlusion(self):
-    fl = Filters([{ 'table': 'T2', 'table_by_id': True }])
-    self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), ['T1'])
+  def test_exclusion_table(self):
+    fl = Filters([{ 'source': 0, 'table': 'T2', 'table_by_id': True }])
+    self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), ['T1', 'T3'])
+  
+  def test_exclusion_table_name(self):
+    fl = Filters([{ 'table': 'T' }])
+    self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), [])
 
-  def test_inclusion(self):
+  def test_inclusion_table(self):
     fl = Filters([{ 'table': 'T2', 'table_by_id': True, 'column': 'fields_b' }], inclusive=True)
     self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), ['T2'])
+
+  def test_inclusion_name(self):
+    fl = Filters([{ 'source': 0, 'table': '1' }], inclusive=True)
+    self.assertSequenceEqual(table_ids(fl.filter(SOURCES)), ['T1'])
 
 if __name__ == '__main__':
   unittest.main()
